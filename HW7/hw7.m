2a17f50ce4fb49c96d8b0f4f8369f20e719fa7df
RobinChen <chienpinchen@gmail.com>
2016-05-26 17:43:31 -0700

    Add function to plot the results.

--- HW7/hw7.m
+++ HW7/hw7.m
@@ -21,7 +21,7 @@ legend('show', 'Location', 'SouthEast');
 
 % setting initial x_0, P_0 
 %x_0 = [40; 0; 200]; % my first guess. (integer according y1(0) and y2(0))
-x_0 = [y(1,1); 1.6; y(1,1)+y(2,1)]; % more reasoning guess (after course)
+x_0 = [y(1,1); 0.2; y(1,1)+y(2,1)]; % more reasoning guess (after course)
 % P_0 = [10 0 0;
 %         0 0 0;    % my first guess. (set as variance of the measurement 
 %         0 0 10];  % for P11 P33. and set others as zeros)
@@ -112,53 +112,60 @@ for k = length(y):-1:1
     P_ks(:,:,k) = P_kp(:,:,k) + A_k*( P_ks(:,:,k+1) - P_kn(:,:,k+1) )*A_k';
 end
 
-% plot expexted X1 and X3 (expect and update)
-t = (0:0.5:100);
-figure;
-plot(t, Ex_arr(1,:), 'LineWidth', 1, 'DisplayName','E\{ Position of the robot (x_1)\} ');
-hold on
-plot(t, Ex_arr(3,:), 'r', 'LineWidth', 1, 'DisplayName','E\{ Position of the wall (x_3)\}');
-set(gca, 'fontsize', 12, 'FontName','Times New Roman');
-title('The estimation of positions of the robot and the wall');
-xlabel ('Time step: k (second)');
-ylabel ('The esitmated positions from the corner (cm)');
+
+% plot the result of the forward Kalman fillter and RTS backward 
+% iterations for the robot position
+t = (0:1:100);
+f1 = figure;        % delcare an empty canvas
+h_f1 = gca;         % get handle value (pointer) of above canvs
+set(h_f1, 'fontsize', 12, 'FontName','Times');
+set(h_f1,'NextPlot', 'add');
+title('The estimated robot position x(t)');
+xlabel('Time step: k (second)');
+ylabel ('The esitmated position from the corner (cm)');
+
+plot(h_f1, t, Ex_kp(1,:), 'DisplayName','Forward: E\{x1\}');
+plot(h_f1, t, Ex_ks(1,:), 'r', 'DisplayName','RTS: E\{x1\}');
 legend('show', 'Location', 'SouthEast');
 
+% plot the result of the forward Kalman fillter and RTS backward 
+% iterations for the robot velocity
+f2 = figure;        % delcare an empty canvas
+h_f2 = gca;         % get handle value (pointer) of above canvs
+set(h_f2, 'fontsize', 12, 'FontName','Times');
+set(h_f2,'NextPlot', 'add');
+title('The estimated robot position dx(t)');
+xlabel('Time step: k (second)');
+ylabel ('The estimated velocity of the robot (cm/s)');
 
-% plot variance X1 and X3 (expect and update)
-var_x1a = reshape(P_arr(1,1,:), 1, []);
-var_x3a = reshape(P_arr(3,3,:), 1, []);
-figure;
-plot(t, var_x1a, 'LineWidth', 1, 'DisplayName','var\{ position of the robot (x_1)\}');
-hold on
-plot(t, var_x3a, 'r', 'LineWidth', 1, 'DisplayName','var\{ position of the wall (x_3)\}');
-set(gca, 'fontsize', 12, 'FontName','Times New Roman');
-title('The vairance of positions of the robot and the wall');
-xlabel ('Time step: k (second)');
-ylabel ('The variance of the positions (cm)');
-legend('show');
-
-
-% plot expexted X1 and X3 (only update)
-figure;
-plot(Ex_kp(1,:), 'DisplayName','Ex1');
-hold on
-plot(Ex_kp(3,:), 'r', 'DisplayName','Ex3');
+plot(h_f2, t, Ex_kp(2,:), 'DisplayName','Forward: E\{x2\}');
+plot(h_f2, t, Ex_ks(2,:), 'r', 'DisplayName','RTS: E\{x2\}');
 legend('show', 'Location', 'SouthEast');
 
-% plot variance X1 and X3
-var_x1 = reshape(P_kp(1,1,:), 1, []);
-var_x3 = reshape(P_kp(3,3,:), 1, []);
-figure;
-plot(var_x1, 'DisplayName','VARx1');
-hold on
-plot(var_x3, 'r', 'DisplayName','VARx3');
-legend('show');
-
-
-% plot smoothing X1 and X3
-figure;
-plot(Ex_ks(1,:), 'DisplayName','smoothing Ex1');
-hold on
-plot(Ex_ks(3,:), 'r', 'DisplayName','smoothing Ex3');
+% plot the result of the forward Kalman fillter and RTS backward 
+% iterations for the wall position
+f3 = figure;        % delcare an empty canvas
+h_f3 = gca;         % get handle value (pointer) of above canvs
+set(h_f3, 'fontsize', 12, 'FontName','Times');
+set(h_f3,'NextPlot', 'add');
+title('The estimated distance from the corner to the wall');
+xlabel('Time step: k (second)');
+ylabel ('The estimated position of the wall (cm)');
+plot(h_f3, t, Ex_kp(3,:), 'DisplayName','Forward: E\{x3\}');
+plot(h_f3, t, Ex_ks(3,:), 'r', 'DisplayName','RTS: E\{x3\}');
 legend('show', 'Location', 'SouthEast');
+
+% plot the variance of the robot position from Kalman filter and Kalman smoother.
+var_x1p = reshape(P_kp(1,1,:), 1, []);
+var_x1s = reshape(P_ks(1,1,:), 1, []);
+f4 = figure;        % delcare an empty canvas
+h_f4 = gca;         % get handle value (pointer) of above canvs
+set(h_f4, 'fontsize', 12, 'FontName','Times');
+set(h_f4,'NextPlot', 'add');
+title('The vairance of the robot position from Kalman filter and smoother');
+xlabel('Time step: k (second)');
+ylabel ('The vairance of the robot position (cm)');
+xlim([-1, 100]);
+plot(h_f4, t, var_x1p, 'DisplayName','Forward: var\{x1\}');
+plot(h_f4, t, var_x1s, 'r', 'DisplayName','RTS: var\{x1\}');
+legend('show', 'Location', 'NorthEast');
