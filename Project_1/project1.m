8b9527a706d45a528c41e573742455831b7ee5d3
RobinChen <chienpinchen@gmail.com>
2016-05-30 16:45:00 -0700

    modified some parameter of discrete EKF to make it work (but need to check if eqn. is correct or not)

--- Project_1/project1.m
+++ Project_1/project1.m
@@ -25,8 +25,8 @@ T = 1/3;   % 30 frames per sec, data are acquired per 10 frames
 %      0, 0, 0, 0];
 
 b = [0 0 1 1]';
-ga = [1 0 0 0;
-      0 1 0 0;
+ga = [0 0 0 0;
+      0 0 0 0;
       0 0 1 0;
       0 0 0 1];
 
@@ -35,12 +35,12 @@ H = [1 0 0 0;
 R = [12.25 0;
      0 12.25]; % do we get this ?? or guess by ourself
 
- Q = 1;
+Q = 12.25^2;
  
 % setting initial x_0, P_0  
 x0 = z_k(1,1);
 y0 = z_k(1,2);
-v0 = 6;     % (z_k(2,1)-z_k(1,1)) / 0.3 
+v0 = 3;     % (z_k(2,1)-z_k(1,1)) / 0.3 
 theta0 = atan2( (z_k(2,2)-z_k(1,2)), (z_k(2,1)-z_k(1,1))  );
 
 Ex0 = [ x0 y0 v0 theta0 ]';   %
@@ -50,10 +50,10 @@ P22 = P11;    % var{y}
 P33 = P11;    % var{v}
 P44 = (180/pi)*2*P11;    % var{theta}
 P12 = 0;
-P13 = 0;
-P14 = 0;
+P13 = P11;
+P14 = P11;
 P23 = 0;
-P24 = 0;
+P24 = P11;
 P34 = 0;
 
 Px0 =[P11 P12 P13 P14;
@@ -82,22 +82,25 @@ tot_X=X_in';
 
 for k = 1 : length(z_k)
     % prediction
+    % Continued method
 %     [t, solXs] = ode45(@dPredictFunc, [(k-1)*T k*T], X_in);
 %     solX = solXs(end,:)';
-%     Ex_k(:,k+1) = [solX(1);  solX(2); solX(3); wrapToPi(solX(4))];
+%     Ex_k(:,k+1) = [solX(1);  solX(2); solX(3); (solX(4))];
 % 
 %     P_k(:,:,k+1) = [solX(5)  solX(9)  solX(10) solX(11); 
 %                  solX(9)  solX(6)  solX(12) solX(13);
 %                  solX(10) solX(12) solX(7)  solX(14);
 %                  solX(11) solX(13) solX(14) solX(8)];
+    
+    % Discrete method
     x3 = Ex_k(3, k);
     x4 = Ex_k(4, k);
-    f = [x3*cos(x4); x3*sin(x4); 0; 0];
-    phi = [0, 0, cos(x4), -x3*sin(x4);
+    f = [x3*cos(x4); x3*sin(x4); 0; 0];  % ? is this correct?
+    phi = [0, 0, cos(x4), -x3*sin(x4);   % ? is this correct?
          0, 0, sin(x4), x3*cos(x4);
-         0, 0, 0, 0;
-         0, 0, 0, 0];
-    Ex_k(:, k+1) = f;
+         0, 0, 1, 0;
+         0, 0, 0, 1];
+    Ex_k(:, k+1) = Ex_k(:, k)+f;         % ? is this correct?
     P_k(:,:,k+1) = phi*P_k(:,:,k)*phi'+ ga*Q*ga';
     
     % observation
@@ -109,7 +112,7 @@ for k = 1 : length(z_k)
     P_k(:,:,k+1) = (eye(4) - K*H)*P_k(:,:,k+1);
     Ex_k(:,k+1) = Ex_k(:,k+1) + K*(z_k(k,:)' - H*Ex_k(:,k+1));
     
-    X_in = [Ex_k(1,k+1); Ex_k(2,k+1); Ex_k(3,k+1); wrapToPi(Ex_k(4,k+1)); 
+    X_in = [Ex_k(1,k+1); Ex_k(2,k+1); Ex_k(3,k+1); (Ex_k(4,k+1)); 
             P_k(1,1,k+1); P_k(2,2,k+1); P_k(3,3,k+1); P_k(4,4,k+1);
             P_k(1,2,k+1); P_k(1,3,k+1); P_k(1,4,k+1); P_k(2,3,k+1);
             P_k(2,4,k+1); P_k(3,4,k+1)];
@@ -126,10 +129,14 @@ end
 t = (0: T: T* length(z_k));
 
 hold on
+plot(Ex_k(1,:),Ex_k(2,:), 'g');
 % plot(tot_X(:,1),tot_X(:,2), 'r')
 % figure
 % plot(tot_T,tot_X(:,3), 'g')
 % figure
 % plot(tot_T,tot_X(:,4), 'k')
 
-plot(Ex_k(1,:),Ex_k(2,:), 'r');
+figure
+plot(t, Ex_k(3,:), 'g')
+figure
+plot(t, Ex_k(4,:), 'k')
