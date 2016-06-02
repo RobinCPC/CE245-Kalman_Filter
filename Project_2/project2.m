90b62e1ffbc9158fe9344fdd6dd0414b75077b69
RobinChen <chienpinchen@gmail.com>
2016-06-02 11:58:32 -0700

    modify state space and equations of the project 2 after discuss with Professor (still not work)

--- Project_2/project2.m
+++ Project_2/project2.m
@@ -12,25 +12,34 @@ load data3D.mat;
 plot3(xm, ym, zm,'DisplayName','xm,ym,zm');
 title('xm,ym,zm');
 
-c = 1;
+%T = 0.1;
+%c = 1e-16;
 g = 9.81;
 
-R = eye(4)*12.25; 
+R = eye(4)*1.225; 
 % [12.25 0;
 %      0 12.25]; % do we get this ?? or guess by ourself
 
-Q = 12.25/2;
+% ga = [0 0 0 0;
+%       0 0 0 0;
+%       0 0 0 0;
+%       sqrt(T) 0 0 0;
+%       0 sqrt(T) 0 0;
+%       0 0 sqrt(T) 0;
+%       0 0 0 sqrt(T)];
+Q = ones(4);
 
 % setting initial x_0, P_0
-x0 = xm(1);
-y0 = ym(1);
-z0 = zm(1);
-pit0 = pitch(1);
-rol0 = roll(1);
-yaw0 = yawm(1);
-Ex0 = [ x0 y0 z0 pit0 rol0 yaw0]';
+x0 = xm(45);
+y0 = ym(45);
+z0 = zm(45);
+dx0 = (xm(45)-xm(44))/dtrec(45);
+dy0 = (ym(45)-ym(44))/dtrec(45);
+dz0 = (zm(45)-zm(44))/dtrec(45);
+yaw0 = yawm(45);
+Ex0 = [ x0 y0 z0 dx0 dy0 dz0 yaw0]';
 
-P11 = 12.25;    % var{x}
+P11 = 1.225*1;    % var{x}
 P22 = P11;      % var{y}
 P33 = P11;      % var{z}
 P12 = 0;        % temp set to zero
@@ -40,47 +49,85 @@ Px0 = eye(length(Ex0)) * P11;
 % Px0 = [P11 P12 P13;
 %        P12 P22 P23;
 %        P13 P23 P33];
-   
+Px0(1,4) = 2*P11/(0.0085^2);
+Px0(4,1) = Px0(1,4);
+Px0(2,5) = 2*P11/(0.0085^2);
+Px0(5,2) = Px0(2,5);
+Px0(3,6) = 2*P11/(0.0085^2);
+Px0(6,3) = Px0(3,6);
 
 % array to storge E{x_k} and P_k after update 
 Ex_k = zeros(length(Ex0), length(xm)+1);
 P_k = zeros(length(Ex0), length(Ex0), length(xm)+1);
 
 % Use Extend Kalman Filter (EKF)
-Ex_k(:,1) = Ex0;
-P_k(:,:,1) = Px0;
+Ex_k(:,45) = Ex0;
+P_k(:,:,45) = Px0;
 
-
-for k = 1 : length(zm)
+% start from 45
+for k = 45 : length(zm)
     % Prediction
     % discrete method
-    pit = Ex_k(4, k);
-    rol = Ex_k(5, k);
-    yaw = Ex_k(6, k);
     
-    f = [c*g*( sin(yaw)*sin(rol) + cos(yaw)*cos(rol)*sin(pit) );
-         c*g*( -1*cos(yaw)*sin(rol) + sin(yaw)*cos(rol)*sin(pit) );
+%     pit = Ex_k(4, k);
+%     rol = Ex_k(5, k);
+%     yaw = Ex_k(6, k);
+
+    x4 = Ex_k(4, k);     
+    x5 = Ex_k(5, k);
+    x6 = Ex_k(6, k);
+    rol = roll(k);
+    pit = pitch(k);
+    yaw = yawm(k);
+    dt = dtrec(k);
+    
+    s = (255/6000) * thrust(k);
+    c0 = 1;
+    c = (0.000409*s^2+0.1405*s-0.099)/c0;
+    
+    f = [x4; x5; x6;
+         c*g*( sin(yaw)*sin(rol) + cos(yaw)*cos(rol)*sin(pit) );
+         c*g*( sin(yaw)*cos(rol)*sin(pit) -cos(yaw)*sin(rol) );
          c*g*cos(rol)*cos(pit) - g;
-         0;
-         0;
-         0];
+         0]*dt; 
+% [c*g*( sin(yaw)*sin(rol) + cos(yaw)*cos(rol)*sin(pit) );
+%  c*g*( -1*cos(yaw)*sin(rol) + sin(yaw)*cos(rol)*sin(pit) );
+%  c*g*cos(rol)*cos(pit) - g;
+%  0;
+%  0;
+%  0];
     
-    phi = [0, 0, 0, c*g*cos(pit)*cos(rol)*cos(yaw),  c*g*(cos(rol)*sin(yaw) - cos(yaw)*sin(pit)*sin(rol)), c*g*(cos(yaw)*sin(rol) - cos(rol)*sin(pit)*sin(yaw)) ;
-           0, 0, 0, c*g*cos(pit)*cos(rol)*sin(yaw),  c*g*(-cos(rol)*cos(yaw) + sin(pit)*sin(rol)*sin(yaw)), c*g*(sin(rol)*sin(yaw) + cos(rol)*cos(yaw)*sin(pit)) ;
-           0, 0, 0,     -c*g*cos(rol)*sin(pit),                     -c*g*cos(pit)*sin(rol),                                          0 ;
-           0, 0, 0, 1, 0, 0;
-           0, 0, 0, 0, 1, 0;
-           0, 0, 0, 0, 0, 1];
+    phi = [0 0 0 dt 0 0 0;
+           0 0 0 0 dt 0 0;
+           0 0 0 0 0 dt 0;
+           0 0 0 0 0 0 0;
+           0 0 0 0 0 0 0;
+           0 0 0 0 0 0 0;
+           0 0 0 0 0 0 1];
+    
+% [0, 0, 0, c*g*cos(pit)*cos(rol)*cos(yaw),  c*g*(cos(rol)*sin(yaw) - cos(yaw)*sin(pit)*sin(rol)), c*g*(cos(yaw)*sin(rol) - cos(rol)*sin(pit)*sin(yaw)) ;
+% 0, 0, 0, c*g*cos(pit)*cos(rol)*sin(yaw),  c*g*(-cos(rol)*cos(yaw) + sin(pit)*sin(rol)*sin(yaw)), c*g*(sin(rol)*sin(yaw) + cos(rol)*cos(yaw)*sin(pit)) ;
+% 0, 0, 0,     -c*g*cos(rol)*sin(pit),                     -c*g*cos(pit)*sin(rol),                                          0 ;
+% 0, 0, 0, 1, 0, 0;
+% 0, 0, 0, 0, 1, 0;
+% 0, 0, 0, 0, 0, 1];
+    ga = [0 0 0 0;
+          0 0 0 0;
+          0 0 0 0;
+          sqrt(dt) 0 0 0;
+          0 sqrt(dt) 0 0;
+          0 0 sqrt(dt) 0;
+          0 0 0 sqrt(dt)];
     
     Ex_k(:, k+1) = Ex_k(:, k)+f;
-    P_k(:,:,k+1) = phi*P_k(:,:,k)*phi'+ eye(length(Ex0))*Q;
+    P_k(:,:,k+1) = phi*P_k(:,:,k)*phi'+ ga*Q*ga';
     
     % Observation
-    H = [ 1, 0, 0, 0, 0, 0;
-      0, 1, 0, 0, 0, 0;
-      0, 0, 1, 0, 0, 0;
-      0, 0, 0, 0, 0, 1];
-    x_k1 = [Ex_k(1,k+1) Ex_k(2,k+1) Ex_k(3,k+1) 0 0 Ex_k(6,k+1)]';
+    H = [ 1, 0, 0, 0, 0, 0, 0;
+          0, 1, 0, 0, 0, 0, 0;
+          0, 0, 1, 0, 0, 0, 0;
+          0, 0, 0, 0, 0, 0, 1];
+    x_k1 = [Ex_k(1,k+1) Ex_k(2,k+1) Ex_k(3,k+1) 0 0 0 Ex_k(7,k+1)]';
     z_k1 = [xm(k) ym(k) zm(k) yawm(k)]';
     
     %update K and P(+)
